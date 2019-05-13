078c7ddf66edc6f04ad95aac2c8b58ec8cda5a72
akmui <akmui0912@gmail.com>
2019-05-13 18:28:16 +0900

    .

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height= 700;
+const INITIAL_COLOR = '##2c2c2c';
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = '##2c2c2c';
+canvas.width = CANVAS_SIZE;
+canvas.height= CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -45,6 +51,32 @@ function onMouseUp(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+ );
+
+function handleRange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
 }
 
 if(canvas){
@@ -52,17 +84,13 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
-Array.from(colors).forEach(color => 
-   color.addEventListener("click", handleColorClick)
-);
-
-
-/* 이건 내가 한거. 동영상보면서 다시 맞춰보자*/
-function handleRange(event){
-    const size = event.target.value;
-    ctx.lineWidth = size;
+if(range){
+    range.addEventListener("input", handleRange);
 }
 
-range.addEventListener("click", handleRange);
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
